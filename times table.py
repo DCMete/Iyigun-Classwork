9c0315077ba0fbd92830c116f17f4c28cd451e6c
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2020-10-05 13:14:57 +0100

    Feedback: Timestable
    I've made some changes and it now runs, but there's still something not right. Can you do some more work on it!

--- times table.py
+++ times table.py
@@ -1,9 +1,20 @@
+### SRC - Can you check your indentation? It should be 4 or 2, not 8!
+
 def multiples(table,start,end,name):
 	print("Hi,",name,"Here is your times table:")
 	for i in range(start,end+1,table):
-		return(start,"x",end,"=",i)
-pupilName = ("What is your name?")
-startNum = int(("Hello,",pupilName,"What is the starting number for your times table?"))
-endNum = int(("And the end number?"))
-table = ("And what table would you like?")
-print(multiples(table,startNum,endNum,pupilName))
+                ### SRC - I don't think you want to return here, but print
+		### return(start,"x",end,"=",i)
+		print(start,"x",end,"=",i)
+
+### SRC you seem to have missed the input statements here?	
+pupilName = input("What is your name?")
+### SRC - you can't use the comma's in an input statement...
+startNum = int(input("Hello," + pupilName + "What is the starting number for your times table?"))
+endNum = int(input("And the end number?"))
+table = int(input("And what table would you like?"))
+
+### SRC - unless your subroutine muliples returns a string, you don't want to print it!!
+
+### print(multiples(table,startNum,endNum,pupilName))
+multiples(table,startNum,endNum,pupilName)
